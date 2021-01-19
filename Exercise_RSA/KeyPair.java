0de5c177f6aac3a4108773639deaf136aa7bae76
Jessica Feder <jessica.feder@student.kyh.se>
2021-01-19 15:59:26 +0100

    Initial commit

---
+++ Exercise_RSA/KeyPair.java
@@ -0,0 +1,31 @@
+package Exercise_RSA;
+
+import java.math.BigInteger;
+
+public class KeyPair implements java.io.Serializable {
+
+    private static final long serialVersionUID = 4L;
+    private BigInteger key;
+    private BigInteger n;
+
+    public KeyPair(BigInteger key, BigInteger n) {
+        this.setKey(key);
+        this.setN(n);
+    }
+
+    public BigInteger getN() {
+        return this.n;
+    }
+
+    public void setN(BigInteger n) {
+        this.n = n;
+    }
+
+    public BigInteger getKey() {
+        return this.key;
+    }
+
+    public void setKey(BigInteger key) {
+        this.key = key;
+    }
+}
---
+++ Exercise_RSA/Main.java
@@ -0,0 +1,77 @@
+package Exercise_RSA;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.math.BigInteger;
+import java.security.SecureRandom;
+
+public class Main {
+
+    public static void saveKey(String fileName, KeyPair key) {
+        try {
+            FileOutputStream fileOut = new FileOutputStream(fileName);
+            ObjectOutputStream out = new ObjectOutputStream(fileOut);
+            out.writeObject(key);
+            out.close();
+            System.out.println("Saved key as " + fileName);
+        } catch (IOException i) {
+            i.printStackTrace();
+        }
+    }
+
+    public static KeyPair readKey(String fileName) {
+        KeyPair key = null;
+        try {
+            FileInputStream fileIn = new FileInputStream(fileName);
+            ObjectInputStream in = new ObjectInputStream(fileIn);
+            key = (KeyPair) in.readObject();
+            in.close();
+            System.out.println("Read key from " + fileName);
+        } catch (IOException | ClassNotFoundException i) {
+            i.printStackTrace();
+        }
+        return key;
+    }
+
+    public static void generateKeys(String fileName, int bitLength) {
+        SecureRandom rand = new SecureRandom();
+
+        BigInteger p = new BigInteger(bitLength / 2, 100, rand);
+        BigInteger q = new BigInteger(bitLength / 2, 100, rand);
+        BigInteger n = p.multiply(q);
+        BigInteger phiN = p.subtract(BigInteger.ONE).multiply(q.subtract(BigInteger.ONE));
+        BigInteger e = new BigInteger("3");
+        while (phiN.gcd(e).intValue() > 1) {
+            e = e.add(new BigInteger("2"));
+        }
+
+        BigInteger d = e.modInverse(phiN);
+        KeyPair publicKey = new KeyPair(e, n);
+        KeyPair privateKey = new KeyPair(d, n);
+        saveKey(fileName + "_pub.key", publicKey);
+        saveKey(fileName + "_priv.key", privateKey);
+
+    }
+
+    public static String encrypt(String message, KeyPair key){
+        return (new BigInteger(message.getBytes())).modPow(key.getKey(), key.getN()).toString();
+    }
+
+    public static String decrypt(String message, KeyPair key){
+        return new String((new BigInteger(message)).modPow(key.getKey(), key.getN()).toByteArray());
+    }
+
+    public static void main(String[] args) {
+        int bitLength = 4096;
+        /*KeyPair publicKey = readKey("Jessica_pub.key");
+        KeyPair privateKey = readKey("Jessica_priv.key");
+        String encrypted = encrypt("Hemligt meddelande", publicKey);
+        String clear = decrypt(encrypted, privateKey);
+        System.out.println(clear);*/
+        generateKeys("Jessica", bitLength);
+
+    }
+}
